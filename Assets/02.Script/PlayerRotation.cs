a06ac81f40441fbb8fcdb19b89e5eb52909746c9
sgchoo <choosg@naver.com>
2023-06-11 16:56:01 +0900

    .

--- Assets/02.Script/PlayerRotation.cs
+++ Assets/02.Script/PlayerRotation.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class PlayerRotation : MonoBehaviour
 {
-    // 구르는 속도, 최저 300으로 설정할 것(그게 제일 자연스러웠음)
     [SerializeField] int rollSpeed = 3;
 
     public Transform tr;
 
-    // 움직임 상태를 표현하는 변수
     bool isMove = false;
 
     Rigidbody rb;
@@ -18,9 +16,6 @@ public class PlayerRotation : MonoBehaviour
     {
         rb = this.GetComponent<Rigidbody>();
     }
-
-    // 움직이지 않는 상태라면, 방향키를 입력받아 그 방향으로 굴리기 
-    // 움직이는 상태라면, update를 중단하기
     void Update()
     {
         if(isMove)return;
@@ -50,13 +45,13 @@ public class PlayerRotation : MonoBehaviour
         isMove = false;
     }
 
-    private void OnCollisionEnter(Collision other) 
-    {
-             if(other.collider.CompareTag("Side1")) Physics.gravity = Vector3.down * 9.8f;
-        //else if(other.collider.CompareTag("Side2")) Physics.gravity = Vector3.left * 9.8f;
-        //else if(other.collider.CompareTag("Side3")) Physics.gravity = Vector3.back * 9.8f;
-        else if(other.collider.CompareTag("Side4")) Physics.gravity = Vector3.back * 9.8f;
-        //else if(other.collider.CompareTag("Side5")) Physics.gravity = Vector3.up * -9.8f;
-        //else if(other.collider.CompareTag("Side6")) Physics.gravity = Vector3.forward * -9.8f;
-    }
+    // private void OnCollisionEnter(Collision other) 
+    // {
+    //          if(other.collider.CompareTag("Side1")) Physics.gravity = Vector3.down * 9.8f;
+    //     //else if(other.collider.CompareTag("Side2")) Physics.gravity = Vector3.left * 9.8f;
+    //     //else if(other.collider.CompareTag("Side3")) Physics.gravity = Vector3.back * 9.8f;
+    //     else if(other.collider.CompareTag("Side4")) Physics.gravity = Vector3.back * 9.8f;
+    //     //else if(other.collider.CompareTag("Side5")) Physics.gravity = Vector3.up * -9.8f;
+    //     //else if(other.collider.CompareTag("Side6")) Physics.gravity = Vector3.forward * -9.8f;
+    // }
 }
