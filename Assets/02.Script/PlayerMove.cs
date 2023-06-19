261fa90d15e18e4eddc49546104a8983151028b4
sgchoo <choosg@naver.com>
2023-06-19 09:45:31 +0900

    ARTest&MoveFix

--- Assets/02.Script/PlayerMove.cs
+++ Assets/02.Script/PlayerMove.cs
@@ -2,51 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//<summary>
+// 플레이어 cube는 앞으로만 이동
+// ray를 쏴서 Edge와의 거리를 계산 후
+// 큐브 다음 면으로 넘어가기
+//</summary>
+
 public class PlayerMove : MonoBehaviour
 {
-    public float speed = 200f;
-
+    //구르는 속도
+    public float speed = 150f;
+    //이동 속도
+    float moveSpeed = 2.0f;
+    //굴러가는 이동 값 체크
     bool isMove = false;
-
+    //Edge Collider 감지 ray 체크
     bool isBorder;
 
     Rigidbody rigid;
 
-    private void Awake() 
-    {
-        
-    }
-
     private void Start() 
     {
-        rigid = GetComponent<Rigidbody>();
-        isMove = true;
-        Invoke("BoolValueInit", 1f);
+        ComponentInit();
     }
 
     void Update()
     {
-        if(isMove)return;
-   
-        StartCoroutine(Rolling(Vector3.forward));
-
-        MeshRenderer mesh = this.GetComponent<MeshRenderer>();
-        mesh.enabled = true;
-
-        BoxCollider coll = this.GetComponent<BoxCollider>();
-        coll.enabled = true;
+        Move();
+        EdgeCheck();
     }
 
-    void BoolValueInit()
+    // 변수 초기화 함수
+    void ComponentInit()
     {
-        isMove = false;
-        this.transform.parent = null;
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // forward이동 및 로테이션 고정 함수
+    void Move()
+    {
+        // 앞으로 이동
+        this.transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        // player 로컬로테이션 고정
+        this.transform.localRotation = Quaternion.Euler(new Vector3(transform.rotation.x, 0.0f, 0.0f));
+    }
+
+    //Edge 감지 ray 함수
+    void EdgeCheck()
+    {
+        Ray ray = new Ray(this.transform.position, Vector3.forward);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, 10.0f))
+        {   
+            //hitInfo와 나 사이의 거리와
+            //중복 체크 방지용 bool값 조건
+            if ((hitInfo.distance < 0.95f) && !isBorder)
+            {
+                // 이동 속도 0으로 만든 후
+                moveSpeed = 0.0f;
+                // 큐브 다음면으로 이동
+                StartCoroutine(Rolling(Vector3.forward));
+            }
+        }
     }
 
     private IEnumerator Rolling(Vector3 dir)
     {
         
         isMove = true;
+        //ray 중복 체크 방지
+        isBorder = true;
 
         float angle = 90;
 
@@ -62,36 +89,12 @@ public class PlayerMove : MonoBehaviour
             yield return null;
         }
 
+        // 이동속도 원복
+        moveSpeed = 2.0f;
         isMove = false;
-    }
 
-    private void OnTriggerEnter(Collider other) 
-    {
-        if(other.transform.CompareTag("Zone") && !isBorder)
-        {
-            StartCoroutine(PauseMove());
-        }
-    }
-
-    IEnumerator PauseMove()
-    {
-        yield return new WaitForSeconds(0.03f);
-
-        isBorder = true;
-        this.transform.SetParent(GameObject.FindGameObjectWithTag("World").transform);
-        this.transform.localRotation = Quaternion.Euler(Vector3.zero);
-        this.transform.localScale = new Vector3(2f, 2f, 2f);
-        speed = 0;
-
-        yield return new WaitForSeconds(1.5f);
-
-        this.transform.parent = null;
-        this.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
-        speed = 40f;
-
-        yield return new WaitForSeconds(1.0f);
-        this.transform.localRotation = Quaternion.Euler(Vector3.zero);
-        speed = 200f;
+        //2초 뒤에 중복 체크 방지용 bool 값 변환
+        yield return new WaitForSeconds(2.0f);
         isBorder = false;
     }
 }
