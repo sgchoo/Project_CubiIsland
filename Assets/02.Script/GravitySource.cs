9d985a9ee69453126e3d2d0c12341e9a64d98b45
sgchoo <choosg@naver.com>
2023-06-20 12:41:18 +0900

    Cubi Move

--- Assets/02.Script/GravitySource.cs
+++ Assets/02.Script/GravitySource.cs
@@ -2,80 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class CustomGravity 
-{
+public static class CustomGravity {
+
 	static List<GravitySource> sources = new List<GravitySource>();
 
-	public static void Register (GravitySource source) 
-	{
-		Debug.Assert(
-			!sources.Contains(source),
-			"Duplicate registration of gravity source!", source);
-
+	public static void Register (GravitySource source) {
 		sources.Add(source);
 	}
 
-	public static void Unregister (GravitySource source) 
-	{
-		Debug.Assert(
-			sources.Contains(source),
-			"Unregistration of unknown gravity source!", source);
-
+	public static void Unregister (GravitySource source) {
 		sources.Remove(source);
 	}
 	
-	public static Vector3 GetGravity (Vector3 position) 
-	{
+	public static Vector3 GetGravity (Vector3 position) {
 		Vector3 g = Vector3.zero;
-
-		for (int i = 0; i < sources.Count; i++) 
-		{
+		for (int i = 0; i < sources.Count; i++) {
 			g += sources[i].GetGravity(position);
 		}
-		
 		return g;
 	}
 
-	public static Vector3 GetGravity (Vector3 position, out Vector3 upAxis) 
-	{
+	public static Vector3 GetGravity (Vector3 position, out Vector3 upAxis) {
 		Vector3 g = Vector3.zero;
-		for (int i = 0; i < sources.Count; i++) 
-		{
+		for (int i = 0; i < sources.Count; i++) {
 			g += sources[i].GetGravity(position);
 		}
-
 		upAxis = -g.normalized;
-
 		return g;
 	}
 
-	public static Vector3 GetUpAxis (Vector3 position) 
-	{
+	public static Vector3 GetUpAxis (Vector3 position) {
 		Vector3 g = Vector3.zero;
-
-		for (int i = 0; i < sources.Count; i++) 
-		{
+		for (int i = 0; i < sources.Count; i++) {
 			g += sources[i].GetGravity(position);
 		}
-
 		return -g.normalized;
 	}
 }
 
 public class GravitySource : MonoBehaviour
 {
-    public virtual Vector3 GetGravity (Vector3 position) 
-    {
+    public virtual Vector3 GetGravity (Vector3 position) {
 		return Physics.gravity;
 	}
 
-	void OnEnable () 
-    {
+	void OnEnable () {
 		CustomGravity.Register(this);
 	}
 
-	void OnDisable () 
-    {
+	void OnDisable () {
 		CustomGravity.Unregister(this);
 	}
 }
--- Assets/02.Script/PlayerMove.cs
+++ Assets/02.Script/PlayerMove.cs
@@ -10,109 +10,40 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    //구르는 속도
-    public float rollSpeed = 100f;
     //이동 속도
-    public float moveSpeed = 2.0f;
+    public float moveSpeed = 0.01f;
     //굴러가는 이동 값 체크
-    bool isMove = false;
-    //Edge Collider 감지 ray 체크
-    bool isBorder;
-    //월드 큐브 트랜스폼
-    public Transform worldCube;
-
-    RaycastHit groundHit;
-
-    Rigidbody rigid;
-
-    private void Start() 
-    {
-        ComponentInit();
-    }
 
     void Update()
     {
         Move();
-        EdgeCheck();
-        GroundRay();
-    }
-
-    // 변수 초기화 함수
-    void ComponentInit()
-    {
-        rigid = GetComponent<Rigidbody>();
+        ChangeRotate();
     }
 
     // forward이동 및 로테이션 고정 함수
     void Move()
     {
         // 앞으로 이동
-        this.transform.position += Vector3.forward * moveSpeed * Time.deltaTime;   
+        this.transform.position += transform.forward * moveSpeed * Time.deltaTime; 
     }
 
-    //Edge 감지 ray 함수
-    void EdgeCheck()
-    {
-        Ray ray = new Ray(this.transform.position, Vector3.forward);
 
+    //플레이어 회전 함수
+    private void ChangeRotate()
+    {
         RaycastHit hitInfo;
 
-        if (Physics.Raycast(ray, out hitInfo, 10.0f))
-        {   
-            //hitInfo와 나 사이의 거리와
-            //중복 체크 방지용 bool값 조건
-            if ((hitInfo.distance < 0.95f) && !isBorder)
-            {
-                // 현재 각도를 x축을 제외한 나머지 각도를 0으로 만들어준다.
-                this.transform.rotation = Quaternion.Euler(this.transform.eulerAngles.x, 0.0f, 0.0f);
-                // 이동 속도 0으로 만든 후
-                moveSpeed = 2.0f;
-                //
-                //this.transform.SetParent(worldCube);
-                // 큐브 다음면으로 이동
-                StartCoroutine(Rolling(Vector3.forward));
-            }
-        }
-    }
+        Vector3 rayDirection = ((-transform.up) + (-transform.forward)).normalized;
 
-    void GroundRay()
-    {
-        Ray ray = new Ray(this.transform.position, Vector3.down);
+        // local포지션의 방향으로 레이 발사.
+        Ray ray = new Ray(transform.position, rayDirection);
 
-        Physics.Raycast(ray, out groundHit, 5.0f);
-    }
-
-    private IEnumerator Rolling(Vector3 dir)
-    {
-        //isMove = true;
-        //ray 중복 체크 방지 변수
-        isBorder = true;
-
-        //<Roll 기능>
-        float angle = 90;
-
-        Vector3 center = transform.position + (dir / 2) * transform.localScale.x + (Vector3.down / 2) * transform.localScale.x;
-
-        Vector3 axis = Vector3.Cross(Vector3.up, dir);
-
-        while (angle > 0)
+        if(Physics.Raycast(ray, out hitInfo, 5f))
         {
-            float rotationAngle = Time.deltaTime * rollSpeed;
-            transform.RotateAround(center, axis, rotationAngle);
-            angle -= rotationAngle;
-            yield return null;
+            Debug.Log(hitInfo.transform.name);
+        
+            // 내 현재 방향에서 hitInfo.normal(법선벡터) 방향으로 회전
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.FromToRotation(Vector3.up, hitInfo.normal), 1f);
         }
-        //</Roll 기능>
-
-        // 이동속도 원복
-        moveSpeed = 2.0f;
-
-        //isMove = false;
-
-        //2초 뒤에 중복 체크 방지용 bool 값 변환
-        yield return new WaitForSeconds(0.8f);
-        //this.transform.parent = null;
-        this.transform.rotation = Quaternion.Euler(groundHit.normal);
-        isBorder = false;
     }
 }
