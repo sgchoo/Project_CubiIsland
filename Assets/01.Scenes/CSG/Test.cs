64f0e2950570784da367042d0fed02763e9e1aa5
sgchoo <choosg@naver.com>
2023-06-21 17:21:30 +0900

    Vuforia Multitarget Data Init

--- Assets/01.Scenes/CSG/Test.cs
+++ Assets/01.Scenes/CSG/Test.cs
@@ -20,7 +20,7 @@ public class Test : MonoBehaviour
         if(curTime > 1.5f)
         {
             angle += 90.0f;
-            transform.DOMove(Vector3.forward * 0.01f, 1.0f).SetRelative();
+            transform.DOLocalMove(Vector3.forward * 0.01f, 1.0f).SetRelative();
             transform.DORotate(new Vector3(angle, 0, 0), 1.0f).SetEase(Ease.Unset);
             curTime = 0;
         }
