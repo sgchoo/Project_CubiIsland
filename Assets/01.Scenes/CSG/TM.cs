098fb35ee04671e9c8fe742b7313c7a68e6202b2
sgchoo <choosg@naver.com>
2023-06-23 16:08:00 +0900

    RotationTest

--- Assets/01.Scenes/CSG/TM.cs
+++ Assets/01.Scenes/CSG/TM.cs
@@ -6,18 +6,8 @@ using DG.Tweening;
 
 public class TM : MonoBehaviour
 {
-    float curTime;
-    int angle;
-
     private void Update() 
     {
-        curTime += Time.deltaTime;
-
-        if(curTime > 1.5)
-        {
-            angle += 90;
-            transform.DORotate(new Vector3(angle, 0, 0), 1);
-            curTime = 0;
-        }
+        this.transform.localRotation = Quaternion.Euler(Vector3.zero);
     }
 }
