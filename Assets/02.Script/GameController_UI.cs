4d92998fe061c7ab58406626378dac1f233fae20
JaeMin Lee <disaster.archivist@gmail.com>
2023-07-05 19:55:08 +0900

    230705_UI_07->08 Scene 화면 전환 및 뷰포리아 인식 업데이트

--- Assets/02.Script/GameController_UI.cs
+++ Assets/02.Script/GameController_UI.cs
@@ -8,6 +8,7 @@ using TMPro;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using UnityEngine.XR.Management;
+using Vuforia;
 
 public class GameController_UI : MonoBehaviour
 {
@@ -24,14 +25,26 @@ public class GameController_UI : MonoBehaviour
 
 
 
+    // 이미지 타겟
+    public GameObject arrayImageTarget;
+    private GameObject imageTarget;
+    //public GameObject multiTarget;
+    //public Transform cube;
+    private bool isDetect;
+    private bool isCameraOn;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        isDetect = false;
+        isCameraOn = false;
         // 씬 이름 체크해서 08.Key씬인 경우 ---------------------
         // UIController_FinalMap의 이미지만 카메라에 인식되도록 하기
         // 인식되면 08.미션창 띄우기
         // 미션 타이틀 text 바꾸기 : 키찾기!
-        if (SceneManager.GetActiveScene().name == "08.FindKeyScene")
+        // if (SceneManager.GetActiveScene().name == "08.FindKeyScene")
+        if (SceneManager.GetActiveScene().name == "07_2.LoadScene")
         {
             FindKeyControl();
             if (GameObject.Find("/Canvas08/PanelSafeArea/PanelGameLoading") != null)
@@ -51,15 +64,17 @@ public class GameController_UI : MonoBehaviour
         // 공통으로 -> 미션창 터치하면 3초뒤 게임 시작하기 / isPlaying = true
         // 게임 시작하면 타임 표시하기
 
-
+        //
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (PanelLoading_UI.ActAR = true)
+        if (!isCameraOn && PanelLoading_UI.ActAR)
         {
             ActImageTarget();
+            isCameraOn = true;
         }
     }
 
@@ -68,7 +83,44 @@ public class GameController_UI : MonoBehaviour
         // 이미지 타겟 켜기
         // XROrigin.SetActive(true);
         // XRSettings.enabled = true;
+
+        int len = arrayImageTarget.transform.childCount;
+        for(int idx = 0; idx < len; idx++)
+        {
+            Transform target = arrayImageTarget.transform.GetChild(idx);
+            if (target.name == UIController2_UI.FinalMap.name)
+            {
+                imageTarget = target.gameObject;
+                imageTarget.SetActive(true);
+            }
+        }
+        if (imageTarget == null)
+        {
+            Debug.Log("Error!!");
+        }
+        
     }
+
+    
+
+    public void FocusedImage()
+    {
+        Debug.Log(imageTarget.GetComponentInParent<ObserverBehaviour>().TargetName + ",  " + UIController2_UI.FinalMap.name);
+        if(imageTarget.GetComponentInParent<ObserverBehaviour>().TargetName == UIController2_UI.FinalMap.name)
+        {
+            SceneManager.LoadScene("08.FindKeyScene");
+            // imageTarget.SetActive(false);
+            // arrayImageTarget.SetActive(false);
+            // // cube.gameObject.SetActive(true);
+            // isDetect = true;
+        }
+        
+    }
+
+    // public void FocusedMultiTarget()
+    // {
+    //     if(isDetect) cube.gameObject.SetActive(true);
+    // }
 
     private void FindKeyControl()
     {
