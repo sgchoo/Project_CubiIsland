8587fdbe1d3bab57fc875f342a73f67de26f3bfb
JaeMin Lee <disaster.archivist@gmail.com>
2023-07-06 14:19:26 +0900

    230706_DEV_Portal -> character/map changeScene Test & applied to Actual Scene

--- Assets/01.Scenes/LJM/PortalTouchBehaviour.cs
+++ Assets/01.Scenes/LJM/PortalTouchBehaviour.cs
@@ -12,12 +12,29 @@ public class PortalTouchBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Debug.Log("STart");
+        // Transform parent = GameObject.Find("CubiWorld_Final").transform.Find("InsideObject").transform.Find("TouchObject");
+        // character = parent.Find("Character");
+        // world = parent.Find("WorldMap");
+        // Debug.Log("NAME : " + character.name + " " + world.name);
     }
 
+    private bool isDetect = false;
     // Update is called once per frame
     void Update()
     {
+        // Debug.Log("UpdateSTart");
+        // if(!isDetect && GameObject.Find("CubiWorld_Final") != null)
+        // {
+        //     Debug.Log("Update");
+        //     Transform parent = GameObject.Find("CubiWorld_Final").transform.Find("InsideObject").transform.Find("TouchObject");
+        //     character = parent.Find("Character").transform;
+        //     world = parent.Find("WorldMap").transform;
+        //     Debug.Log("NAME2 : " + character.name + " " + world.name);
+        //     isDetect = true;
+        // }
+        
+
         // 한 번만 누를 때 활성화돰
         if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         {
