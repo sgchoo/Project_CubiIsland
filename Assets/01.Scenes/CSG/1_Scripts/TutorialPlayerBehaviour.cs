90ee6ab6a9794c350f75d35d40be03386de62685
sgchoo <choosg@naver.com>
2023-07-17 11:15:33 +0900

    230717_dev_FX Design_WorldMap&Portal FX Design Complete

--- Assets/01.Scenes/CSG/1_Scripts/TutorialPlayerBehaviour.cs
+++ Assets/01.Scenes/CSG/1_Scripts/TutorialPlayerBehaviour.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class TutorialPlayerBehaviour : MonoBehaviour
 {
+    public GameObject ps;
+
     private void OnTriggerEnter(Collider other) 
     {
         if(other.transform.tag == "Key")
         {
             TutorialGameManager.getKeyCount++;
+            Instantiate(ps, other.transform.position + new Vector3(0, 0.01f, 0), Quaternion.Euler(-90, 0, 0));
             Destroy(other.gameObject);
         }
 
